0995fe6ac2f699e7f234e66a6b96d6189cafb2fa
Jithin Raju <jithin971@gmail.com>
2022-04-30 07:18:25 +0530

    first commit

---
+++ app3/src/App.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import CounterAppThree from "./components/CounterAppThree";
+
+const App = () => (
+	<Box margin="1.2rem">
+		<Box>APP-</Box>
+		<Box>
+			<CounterAppThree />
+		</Box>
+	</Box>
+);
+
+export default App;
+3
---
+++ app3/src/components/CounterAppThree.tsx
@@ -0,0 +1,13 @@
+import { Text, Button, Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const Counter = () => {
+
+  return (
+    <Flex color="#000" gap="1rem" direction="column">
+      <Button>Click me</Button>
+    </Flex>
+  );
+};
+
+export default Counter;
